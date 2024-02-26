9c8888eb67a2e4d3759208d5e6914c39c5f70d9f
Eemeli Miettinen <eemeli.miettinen@methics.fi>
2024-02-26 16:10:59 +0200

    Fix build

--- src/main/java/fi/methics/laverca/rest/MssClient.java
+++ src/main/java/fi/methics/laverca/rest/MssClient.java
@@ -191,10 +191,6 @@ public class MssClient {
         }
     }
     
-    public MSS_SignatureResp batchSign() {
-        
-    }
-    
     /**
      * Sign data. This is typically used for document signing, etc.
      * By default this method returns a CMS signature byte[].
